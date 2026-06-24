3ffb441835c478d4f75746be8900f313d5c8fcc0
Luis Boscan <lboscannava@gmail.com>
2026-06-24 13:14:52 -0500

    Initial commit

---
+++ test/QuiniPool.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {QuiniPool} from "../src/QuiniPool.sol";
+
+contract QuiniPoolTest is Test {
+    QuiniPool public quiniPool;
+
+    function setUp() public {
+        quiniPool = new QuiniPool();
+    }
+}
